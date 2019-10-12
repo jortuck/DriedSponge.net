eb05c20f3a9ba6b5aca9e6acadb23245aece7ffd
Jordan Tucker <jortuck0810@gmail.com>
2019-10-12 11:36:47 -0700

    Added google analytics

--- meta.php
+++ meta.php
@@ -1,1 +1,10 @@
 <meta name="theme-color" content="#007BFF">
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140402227-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-140402227-3');
+</script>
