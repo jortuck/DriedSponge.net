6306ed8c669f5442c55663c8dd750bfa319b03ff
Justin Klüver <justin.k@justplayer.de>
2019-12-12 11:17:34 +0100

    We actually dont need to do a query if we check your steamid

--- src/libs/functions.php
+++ src/libs/functions.php
@@ -4,9 +4,13 @@
 
 // Returns true if the user is YOUR_STEAMID or is found in database
 function isAdmin($steamid) {
+    if ($steamid == "76561198357728256") {
+        return true;
+    }
+
     $isAdminQuery = SQLWrapper()->prepare("SELECT id64 FROM staff WHERE id64 = ?"); // because its a single var we can use ?
     $isAdminQuery->execute([$steamid]);
     $Admin = $isAdminQuery->fetch();
 
-    return ($steamid == "76561198357728256" || !empty($Admin));
+    return !empty($Admin);
 }
