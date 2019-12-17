2f0d15434e824790d0e702272e67a623288f17ab
DriedSponge <jortuck0810@gmail.com>
2019-12-16 20:59:11 -0800

    Fixed some responsivenss I guyss

--- hex.php
+++ hex.php
@@ -1,10 +1,10 @@
-<div class="text-center">
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
+<div class="container text-center">
+    <div id="carouselExampleSlidesOnly" class="carousel slide img-fluid" data-ride="carousel">
+    <div class="carousel-inner img-fluid" >
         <div class="carousel-item active">
         <a href="https://billing.hexanenetworks.com/aff.php?aff=330" target="_blank"style="text-align: center;"><img class="img-fluid" style="max-width:600px;"  src="https://cdn.hexanenetworks.com/img/web/branding/hexane-networks-banner.png" /></a>
         </div>
-        <div class="carousel-item">
+        <div class="carousel-item ">
         <a href="https://m.do.co/c/bdc37dd1f345" target="_blank"style="text-align: center;"><img class="img-fluid" style="max-width:600px;"  src="https://driedsponge.net/img/do_banner.png" /></a>
         </div>
     </div>
