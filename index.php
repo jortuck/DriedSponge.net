23f86d87902234e4127964a103c6118c5f7c18bc
DriedSponge <jortuck0810@gmail.com>
2020-01-07 21:53:48 -0800

    Fixed and fucked up some responsiveness

--- index.php
+++ index.php
@@ -66,33 +66,33 @@ $mcreatedbyurl = "https://steamcommunity.com/profiles/".$mcreatedby."/";
                             <div class="row justify-content-center">
                                 <div  class="card card-border"  data-aos="fade-up" style="width: 20%; text-align:center;">
                                 <div class="card-body">
-                                <img  src="img/html.png" alt="html5logo" height="100px" style="padding: none; margin: none;"/>
+                                <img  src="img/html.png" alt="html5logo" class="img-fluid" style="max-height:100px; padding: none; margin: none;"/>
                                 <br>
                                 </div>
                                 </div>
                                 
                                 <div data-aos="fade-up" class="card card-border" style="width: 20%; text-align:center;">
                                         <div class="card-body">
-                                        <img  src="img/lua.png" alt="lualogo" height="100px" style="padding: none; margin: none;"/>
+                                        <img  src="img/lua.png" alt="lualogo" height="100px" class="img-fluid" style="max-height:100px; padding: none; margin: none;"/>
                                             
                                         </div>
                                         </div>
                                  <div data-aos="fade-up" class="card card-border" style="width: 20%; text-align:center;">
                                                 <div class="card-body">
-                                                <img  src="img/php.png" alt="phplogo" height="100px" style="padding: none; margin: none;"/>
+                                                <img  src="img/php.png" alt="phplogo" height="100px" class="img-fluid" style="max-height:100px; padding: none; margin: none;"/>
                                                     
                                                 </div>       
                                                 </div>
                                   <div  data-aos="fade-up" class="card card-border" style="width: 20%; text-align:center;">
                                                     
                                             <div class="card-body">
-                                            <img   src="img/css.png" alt="csslogo" height="100px" style="padding: none; margin: none;"/>
+                                            <img   src="img/css.png" alt="csslogo" height="100px" class="img-fluid" style="max-height:100px; padding: none; margin: none;"/>
                                                  
                                             </div>
                                         </div>              
                                   <div data-aos="fade-up" class="card card-border" style="width: 20%; text-align:center;">
                                                     <div class="card-body">
-                                                <img  src="img/js.png" alt="jslogo" height="100px" style="padding: none; margin: none;"/>
+                                                <img  src="img/js.png" alt="jslogo" height="100px" class="img-fluid" style="max-height:100px; padding: none; margin: none;"/>
                                                                     
                                                 </div>
                                             </div>                      
--- search.php
+++ search.php
@@ -1,7 +1,6 @@
 <form method="GET" action="controller.php">
-<div class="form-group">
-                        
-     <input id="id64" name="id" type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter a SteamID/SteamID64/SteamID3/Profile URL/Custom Profile URL" required>
+    <div class="form-group">
+        <input id="id64" name="id" type="text" class="form-control"  aria-describedby="emailHelp" placeholder="Enter a SteamID/SteamID64/SteamID3/Profile URL/Custom Profile URL" required>
     </div>
-<button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary">Submit</button>
 </form>
