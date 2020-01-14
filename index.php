415af38aef25bc5a00dedf2b299e24a52ef0b016
DriedSponge <jortuck0810@gmail.com>
2020-01-14 14:18:45 -0800

    fixed small redirect bug

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require "Bramus/Router/Router.php";
 include ("databases/connect.php");
 include ("src/libs/functions.php");
-require('steamauth/steamauth.php');  
+
 $router = new \Bramus\Router\Router();
 
 $router->get('pattern', function() { /* ... */ });
@@ -20,108 +20,134 @@ $router->set404(function () {
 });
 //Indexs
 $router->all('/', function () {
-    $header = "/";
+    $header = "/home";
+    require('steamauth/steamauth.php');  
     include('views/index.php');
 });
 $router->all('/index.php', function () {
-    $header = "/index.php";
+    $header = "/home";
+    require('steamauth/steamauth.php');  
     include('views/index.php');
 });
 $router->all('/home', function () {
-    $header = "/home/";
+    $header = "/home";
+    require('steamauth/steamauth.php');  
     include('views/index.php');
 });
 //Static pages
 $router->all('/projects/web', function () {
     $header = "/web/";
+    require('steamauth/steamauth.php');  
     include('views/webdesign.php');
 });
 $router->all('/projects/lua', function () {
     $header = "/lua/";
+    require('steamauth/steamauth.php');  
     include('views/lua.php');
 });
 //User pages(pages where user can interacts)
 $router->all('/advertise', function () {
     $header = "/advertise/";
+    require('steamauth/steamauth.php');  
     include('views/advertise.php');
 });
 $router->get('/advertise/{action}', function ($action) {
     $header = "/advertise/".$action;
+    require('steamauth/steamauth.php');  
     include('views/advertise.php');
 });
 $router->all('/feedback', function () {
     $header = "/feedback/";
+    require('steamauth/steamauth.php');  
     include('views/feedback.php');
 });
 $router->get('/feedback/{action}', function ($action) {
     $header = "/feedback/";
+    require('steamauth/steamauth.php');  
     include('views/feedback.php');
 });
 $router->all('/verify', function () {
     $header = "/verify/";
+    require('steamauth/steamauth.php');  
     include('views/verify.php');
 });
 $router->get('/verify/{id}', function ($verify) {
     $header = "/verify/".$verify;
+    require('steamauth/steamauth.php');  
     include('views/verify.php');
 });
 //Steam routes
 $router->all('/lookup/{id}', function ($id) {
     $header = "/lookup/$id";
+    require('steamauth/steamauth.php');  
     include('views/controller.php');
 });
 $router->get('/steam', function () {
     $header = "/steam/";
+    require('steamauth/steamauth.php');  
     include('views/steam.php');
+
  });
  $router->post('/lookup/', function () {
     $header = "/lookup/".$_POST['id'];
+    require('steamauth/steamauth.php');  
     header("Location: /lookup/".$_POST['id']);
  });
  $router->all('/steam/error', function () {
     $header = "/steam/error";
+    require('steamauth/steamauth.php');  
     include('views/steamerror.php');
 });
 //Management pages
 $router->all('/manage', function () {
     $header = "/manage/home";
+    require('steamauth/steamauth.php');  
     include('views/manage/index.php');
 });
 $router->all('/manage/home', function () {
     $header = "/manage/home";
+    require('steamauth/steamauth.php');  
     include('views/manage/index.php');
 });
 $router->all('/manage/index.php', function () {
     $header = "/manage/home";
+    require('steamauth/steamauth.php');  
     include('views/manage/index.php');
 });
 $router->all('/manage/users', function () { //Users Manage
     $header = "/manage/users/";
+    require('steamauth/steamauth.php');  
     include('views/manage/users.php');
 });
 $router->get('/manage/users/{action}', function ($action) { //Users Manage Action
     $header = "/manage/users/";
+    require('steamauth/steamauth.php');  
     include('views/manage/users.php');
 });
 $router->all('/manage/content', function () { //Cotnent Manage
     $header = "/manage/content/";
+    require('steamauth/steamauth.php');  
     include('views/manage/content.php');
 });
 $router->get('/manage/content/{action}', function ($action) { //Content Manage Actions
     $header = "/manage/content/";
+    require('steamauth/steamauth.php');  
     include('views/manage/content.php');
 });
 $router->get('/manage/edit/{pageid}/{action}', function ($pageid,$action) { //Editor Manage Actions
     $header = "/manage/edit/".$pageid;
+    require('steamauth/steamauth.php');  
     include('views/manage/editor.php');
 });
 $router->all('/manage/edit/{pageid}', function ($pageid) { //Editor Manage
     $header = "/manage/edit/".$pageid;
+    require('steamauth/steamauth.php');  
     include('views/manage/editor.php');
 });
 
 $router->all('/{slug}/', function ($slug) { //Editor Manage
     $header = "/".$slug."/";
+    require('steamauth/steamauth.php');  
     include('views/page.php');
 });
 
