e1755c2e074df123d72393bbe68e35df785313b2
DriedSponge <jortuck0810@gmail.com>
2020-01-20 02:06:48 +0000

    Revert "Merge branch 'revert-82caa5d3' into 'master'"
    This reverts merge request !3

--- databases/admin.php
+++ databases/admin.php
@@ -1,5 +1,11 @@
 <?php
 include("connect.php");
+/**
+ * Check if the given SteamID64 has Admin permissiosn
+ *
+ * @param string $isadminid
+ * @return void
+ */
 function isadmin($isadminid){
 $isadminidquery = $conn->prepare("SELECT id64 FROM staff WHERE id64 = :id");
 $isadminidquery->execute([':id' => $isadminid]);
