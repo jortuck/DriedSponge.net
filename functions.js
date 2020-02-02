b3f94df1479a4f0abf6f6aebb2b4a75b2e5f484d
DriedSponge <jortuck0810@gmail.com>
2020-02-02 00:14:26 -0800

    Added Loading funtion to functions.js

--- functions.js
+++ functions.js
@@ -13,4 +13,11 @@
          $(feedbackid).html(msg);
 		
     }
-  
+  function Loading(show,id){
+      if(show){
+        $(id).removeClass("d-none");
+        $(id).html(`<div class="text-center"><div class="spinner-border text-primary " role="status"><span class="sr-only">Loading...</span></div><br><p class="paragraph"><b>Loading...</b></p></div>`);
+      }else{
+        $(id).addClass("d-none");
+      }
+  }
