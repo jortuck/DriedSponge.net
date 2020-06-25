540ae6a9d6e450c55fe73cd93173df0bd32ae80f
DriedSponge <jortuck0810@gmail.com>
2020-06-25 01:21:27 -0700

    Added custom banners for displaying critical information

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,9 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('inc.head')
     <body style="background: #0E1013;">
+    <div class="banner banner-success">
+        <p><b>This site is currently in development and is currently unfinished! If you come across any bugs or you have a suggestion please <a href="mailto:driedsponge@driedsponge.net" target="_blank">email me</a>.</b>
+    </div>
     <br>
         @include('inc.navbar')
     <div>
