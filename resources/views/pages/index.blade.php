c224390c75a906c856d59b64e11356b230014db6
DriedSponge <jortuck0810@gmail.com>
2020-07-07 02:44:33 -0700

    Some work

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -8,19 +8,20 @@
     <div>
         <div class="container" id="landing-header">
             <div><h1 data-aos="fade-down" class="landing-h1 center-align">Dried <span class="white-text">Sponge</span></h1></div>
-            <h2 data-aos="fade-up" data-aos-delay="500" class="landing-sub center-align">Full Stack Web Developer</h2>
-            <h2 data-aos="fade-up" data-aos-delay="500" class="center-align">
-                <a class="icon-link" target="_blank" href="mailto:jordan@driedsponge.net"><i class="fas fa-envelope"></i></a>
+            <h2 class="landing-sub center-align">Full Stack Web Developer</h2>
+            <h2 class="center-align">
                 <a class="icon-link" target="_blank" href="https://github.com/DriedSponge"><i class="fab fa-github"></i></a>
                 <a class="icon-link" target="_blank" href="https://steamcommunity.com/id/driedsponge/"><i class="fab fa-steam"></i></a>
                 <a class="icon-link" target="_blank" href="https://twitter.com/dried_sponge"><i class="fab fa-twitter"></i></a>
                 <a class="icon-link" target="_blank" href="https://discord.gg/YS4WZWG"><i class="fab fa-discord"></i></a>
-
             </h2>
         </div>
     </div>
     <br>
-    <div class="section" style="z-index: 900; position: relative; top: 175px">
+    <br>
+    <div class="section" style="position: relative;">
+        <h2 class="center-align white-text" style="font-weight: 600">WHAT I DO</h2>
+        <br>
         <div class="container">
             <div class="row">
                 <div class="col s12 m6 l4">
@@ -50,7 +51,37 @@
             </div>
         </div>
     </div>
-    <div style="width: 100%;height: 300px;background: #62A1EC;">
+    <div class="section" style="background: #62A1EC;">
+        <h2 class="center-align white-text" style="font-weight: 600;">SERVICES I OFFER</h2>
+        <br>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m6 l4">
+                    <div class="hoverable card-panel center-align">
+                        <h1>Backend Development</h1>
+                        <p class="flow-text">I have expierience with lots of backend development including
+                            laravel, python and node js.
+                        </p>
+                    </div>
+                </div>
+                <div class="col s12 m6 l4">
+                    <div class="hoverable card-panel center-align">
+                        <h1>Server Infrastructure</h1>
+                        <p class="flow-text">Web services require servers and databases. Over the years I
+                            have aquried the knowlege to manage these types of services.
+                        </p>
+                    </div>
+                </div>
+                <div class="col s12 m12 l4">
+                    <div class="hoverable card-panel center-align">
+                        <h1>Web Design</h1>
+                        <p class="flow-text">
+                            Using HTML, CSS, and JavaScript, I can transform your design into a beautiful webpage with a great, responsive, user interface.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <script>
         const observer = lozad(); // lazy loads elements with default selector as '.lozad'
