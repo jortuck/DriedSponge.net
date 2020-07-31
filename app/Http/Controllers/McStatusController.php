5ca4f5a237c8a720cd3f554132929c8be4a786cc
DriedSponge <jortuck0810@gmail.com>
2020-07-31 01:10:38 -0700

    Changed direcotries to logs

--- app/Http/Controllers/McStatusController.php
+++ app/Http/Controllers/McStatusController.php
@@ -21,12 +21,12 @@ class McStatusController extends Controller
         {
             $Query = array('success'=>false);
         }
-        $log = preg_replace('/([0-9]+\\.[0-9]+\\.[0-9]+)\\.[0-9]+/', 'XX.XX.XX.XX', file_get_contents('/home/driedsponge/Servers/Minecraft/1.16.1/logs/latest.log'));
+        $log = preg_replace('/([0-9]+\\.[0-9]+\\.[0-9]+)\\.[0-9]+/', 'XX.XX.XX.XX', file_get_contents('/home/driedsponge/servers/mc/1.16.1/logs/latest.log'));
         $log ='test';
         return view('pages.mc')->with('query', $Query)->with('log',$log);
     }
     public function log(){
-        $log = preg_replace('/([0-9]+\\.[0-9]+\\.[0-9]+)\\.[0-9]+/', 'XX.XX.XX.XX', file_get_contents('/home/driedsponge/Servers/Minecraft/1.16.1/logs/latest.log'));
+        $log = preg_replace('/([0-9]+\\.[0-9]+\\.[0-9]+)\\.[0-9]+/', 'XX.XX.XX.XX', file_get_contents('/home/driedsponge/servers/mc/1.16.1/logs/latest.log'));
         $log ='test';
         $response = Response::make($log, 200);
         $response->header('Content-Type', 'text/plain');
