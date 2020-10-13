9580c60c73095b5144ddfb76217ffbaa407e95d6
DriedSponge <jortuck0810@gmail.com>
2020-10-13 12:26:23 -0700

    Got basics of auth down for github webhook

--- app/Http/Controllers/Api/Github.php
+++ app/Http/Controllers/Api/Github.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\ApiKey;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class Github extends Controller
 {
-    function Webhook(){
-        return response()->json("Test");
+    function Webhook(Request $request){
+        $apikey = $request->header('HTTP_X_HUB_SIGNATURE');
+        if($apikey!=''){
+            if (ApiKey::authed($request->get('api_token'))) {
+                $key = ApiKey::where('api_token', $request->get('api_token'))->first();
+                if ($key->hasPermissionTo('GitHubWebhook', 'api')) {
+                    return response()->json(['success' => false,'message' => 'adadadad'],200);
+                }else{
+                    return response()->json(['success' => false,'message' => 'Authenticated'],403);
+                }
+            } else {
+                return response()->json(['success' => false, 'message' => 'Unauthorized'],403);
+            }
+        }
+        return response()->json(['success' => false,'message' => 'Unauthenticated'],401);
     }
 }
