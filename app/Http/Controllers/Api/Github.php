ddef0b84916d594fed9f8959e44e693f8867b7f3
DriedSponge <jortuck0810@gmail.com>
2020-10-14 15:13:07 -0700

    Fixed the thing not working

--- app/Http/Controllers/Api/Github.php
+++ app/Http/Controllers/Api/Github.php
@@ -10,17 +10,16 @@ class Github extends Controller
 {
     function Webhook(Request $request)
     {
-        $apikey = $request->header('HTTP_X_HUB_SIGNATURE');
+        $apikey = $request->header('X-Hub-Signature');
         if ($apikey != null) {
             $postBody = file_get_contents('php://input');
             if ($apikey == 'sha1=' . hash_hmac('sha1', $postBody, env('GITHUB_WEBHOOK_SECRET'), false)) {
                 if ($request->post('payload') != null) {
                     $data = json_decode($request->post('payload'), true);
-                    $config = $data['config'];
                     $repo = $data['repository'];
                     $owner = $repo['owner'];
                     $sender = $data['sender'];
-                    switch ($request->header('HTTP_X_GITHUB_EVENT')) {
+                    switch ($request->header('X-GitHub-Event')) {
                         case "push":
                             $commits = $data['commits'];
                             $fields = array();
@@ -45,16 +44,16 @@ class Github extends Controller
                                     "fields" => $fields,
                                     "footer" => array("text" => "Powered by myself")
                                 );
-                                return response()->json(['success' => true, 'message' => 'No payload attached'], 200);
+                                return response()->json(['success' => true, 'message' => 'Push webhook success'], 200);
 
                             }
                     }
                 }
                 return response()->json(['success' => false, 'message' => 'No payload attached'], 200);
             } else {
-                return response()->json(['success' => false, 'message' => 'Authenticated'], 403);
+                return response()->json(['success' => false, 'message' => 'Unauthorized'], 403);
             }
         }
-        return response()->json(['success' => false, 'message' => 'Unauthenticated'], 401);
+        return response()->json(['success' => false, 'message' =>  "Unauthenticated"], 401);
     }
 }
