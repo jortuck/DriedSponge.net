2ce452037f41b12fb96a122411574a8b633a77a4
DriedSponge <jordan@driedsponge.net>
2020-11-12 10:48:06 -0800

    Fixed building stuff, hopefully make it so I don't have to build every time

---
+++ src/vue/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="section">
+      <Pagehead v-bind:sub_header="subhead" >
+        <span style="color: #62A1EC">DRIED</span> SPONGE
+      </Pagehead>
+  </section>
+</template>
+<script>
+import Pagehead from "@/components/Pagehead";
+export default {
+  name: 'Home',
+  components: {Pagehead},
+  data: function () {
+    return {
+      subhead:"Full Stack Web Developer"
+    }
+  },
+
+}
+</script>
