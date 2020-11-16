04a6d5128a2d7945597ca3879f2f8a7cd6fca438
DriedSponge <jordan@driedsponge.net>
2020-11-16 10:28:50 -0800

    Removed useless route

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 Route::get('/source-query/all', 'Api\SourceQueryApi@GetAll')->middleware('ApiKey');
 Route::get('/source-query/info', 'Api\SourceQueryApi@Info')->middleware('ApiKey');
 Route::get('/walrus/facts', 'Api\Walrus@Facts')->middleware('ApiKey');
@@ -25,4 +22,3 @@ Route::post('/test/post',function (){
     sleep(3);
     return response()->json(["test"=>"test","test2"=>"test2"]);
 });
-
