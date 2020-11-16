032a81faf32c448c29e57f77d3c1c9e3b79cfa94
DriedSponge <jordan@driedsponge.net>
2020-11-16 10:41:16 -0800

    Removed useless comments

--- src/app/Http/Controllers/Auth/SteamLoginController.php
+++ src/app/Http/Controllers/Auth/SteamLoginController.php
@@ -35,8 +35,5 @@ class SteamLoginController extends AbstractSteamLoginController
         }
         // login the user using the Auth facade
         Auth::login($user);
-        //Cookie::queue("session", $token, "60");
-        //$token = $user->createToken("token")->plainTextToken;
-        // let the extended controller handle redirect back to the page the user was just on
     }
 }
