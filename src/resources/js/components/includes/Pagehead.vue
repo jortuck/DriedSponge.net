6f14355898bf8e58bc5e75f089c09f47f0c964c5
DriedSponge <jordan@driedsponge.net>
2020-11-29 11:23:13 -0800

    Formated Pagehead

--- src/resources/js/components/includes/Pagehead.vue
+++ src/resources/js/components/includes/Pagehead.vue
@@ -1,29 +1,25 @@
 <template>
-  <div class="has-text-centered container" data-aos="fade-in">
-    <h1 class="title page-title mb-1">
-      <slot></slot>
-      {{ header }}
-    </h1>
-    <h2 class="subtitle page-subtitle mt-2" v-if="sub_header">{{ sub_header }}</h2>
-  </div>
+    <div class="has-text-centered container" data-aos="fade-in">
+        <h1 class="title page-title mb-1">
+            <slot></slot>
+            {{ header }}
+        </h1>
+        <h2 class="subtitle page-subtitle mt-2" v-if="sub_header">{{ sub_header }}</h2>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "Pagehead",
-  props: {
-    header: {
-      type: String,
-      required: false
-    },
-    sub_header: {
-      type: String,
-      required: false
-    },
+    name: "Pagehead",
+    props: {
+        header: {
+            type: String,
+            required: false
+        },
+        sub_header: {
+            type: String,
+            required: false
+        },
     }
 }
 </script>
-
-<style scoped>
-
-</style>
