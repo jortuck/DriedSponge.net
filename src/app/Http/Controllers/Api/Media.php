699b878cd33ccf45e9968df19fb692d91f724be8
DriedSponge <jordan@driedsponge.net>
2020-11-30 11:04:35 -0800

    File saving done

--- src/app/Http/Controllers/Api/Media.php
+++ src/app/Http/Controllers/Api/Media.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\SharexMedia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
 class Media extends Controller
@@ -12,15 +13,19 @@ class Media extends Controller
     public function upload(Request $request){
         $file = $request->file('image');
         $extention = $file->extension();
+        $name = $request->file('image')->getClientOriginalName();
         if($file->getSize()<99000000) {
             $upload = new SharexMedia();
-            $upload->name = $request->file('image')->getClientOriginalName();
+            $upload->name = $name;
             $upload->type = $extention;
             $upload->uuid = Str::random(10);
             $upload->save();
+            $file->storeAs(
+                "/media/$extention", $name
+            );
 
             return $upload->uuid;
         }
-        return "TOOBIG";
+        return response()->json(["success"=>false,"error"=>"The file you tried to send is too big",""]);
     }
 }
