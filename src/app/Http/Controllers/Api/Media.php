7d1193aa2ec9f179641c36c15e7e45d88cd6836e
DriedSponge <jordan@driedsponge.net>
2020-11-30 17:09:09 -0800

    Added authorization for image uploader

--- src/app/Http/Controllers/Api/Media.php
+++ src/app/Http/Controllers/Api/Media.php
@@ -13,24 +13,29 @@ class Media extends Controller
 {
     public function upload(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            "image" => "required|file|max:99000|mimes:jpeg,webp,png,mp4,gif,mov"
-        ]);
-        if ($validator->passes()) {
-            $file = $request->file('image');
-            $extention = $file->extension();
-            $name = Str::random(10);
-            $upload = new SharexMedia();
-            $upload->name = $name . '.' . $extention;
-            $upload->type = $extention;
-            $upload->uuid = $name;
-            $upload->save();
-            $file->storeAs(
-                "/sharex/$extention", $upload->name
-            );
-            return response()->json(["success" => true, "id" => $upload->uuid, "url" => route('media.load-view', $upload->uuid), "raw_url" => route('media.load-file', $upload->uuid)]);
-
+        if($request->header('Authorization')){
+            if($request->header('Authorization') == "Bearer ".config("extra.image_secret")){
+                $validator = Validator::make($request->all(), [
+                    "image" => "required|file|max:99000|mimes:jpeg,webp,png,mp4,gif,mov"
+                ]);
+                if ($validator->passes()) {
+                    $file = $request->file('image');
+                    $extention = $file->extension();
+                    $name = Str::random(10);
+                    $upload = new SharexMedia();
+                    $upload->name = $name . '.' . $extention;
+                    $upload->type = $extention;
+                    $upload->uuid = $name;
+                    $upload->save();
+                    $file->storeAs(
+                        "/sharex/$extention", $upload->name
+                    );
+                    return response()->json(["success" => true, "id" => $upload->uuid, "url" => route('media.load-view', $upload->uuid), "raw_url" => route('media.load-file', $upload->uuid)]);
+                }
+                return response()->json(["success" => false, "error" => "Invalid File","errors"=>$validator->errors()],500);
+            }
+            return  response()->json(["success"=>false,"error"=>"Unauthorized","url"=>"Unauthorized"],403);
         }
-        return response()->json(["success" => false, "error" => "Invalid File","errors"=>$validator->errors()],500);
+        return  response()->json(["success"=>false,"error"=>"Unauthenticated","url"=>"Unauthenticated"],401);
     }
 }
