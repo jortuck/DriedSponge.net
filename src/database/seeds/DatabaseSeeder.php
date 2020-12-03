616eec175d7dab04d229dd212d79556c1e2e5de0
DriedSponge <jordan@driedsponge.net>
2020-12-02 16:09:31 -0800

    Seeders now only break half the time ;)

--- src/database/seeds/DatabaseSeeder.php
+++ src/database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call([UploadSeeder::class]);
         $this->call([RolesAndPermissionsSeeder::class]);
         $this->call([UserSeeder::class]);
-        $this->call([UploadSeeder::class]);
     }
 }
--- src/database/seeds/UserSeeder.php
+++ src/database/seeds/UserSeeder.php
@@ -15,11 +15,17 @@ class UserSeeder extends Seeder
         $key = config("steam-login.api_key");
         $file = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=$key&steamids=76561198357728256");
         $steamUser = json_decode($file)->response->players[0];
-        $user = User::create([
-            'username' => "DriedSponge",
-            'steamid' => $steamUser->steamid,
-            'avatar' => $steamUser->avatarfull
-        ]);
-        $user->assignRole('Owner');
+        if(!User::where('steamid',$steamUser->steamid)->first()){
+            $user = User::create([
+                'username' => "DriedSponge",
+                'steamid' => $steamUser->steamid,
+                'avatar' => $steamUser->avatarfull
+            ]);
+            $user->assignRole('Owner');
+            echo "\nSeeded DriedSponge\n";
+        }else{
+            echo "\nNo user to seed\n";
+        }
+
     }
 }
