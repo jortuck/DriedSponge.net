39b26ef011504840d38fccb4ca775b71b3184a7f
DriedSponge <jordan@driedsponge.net>
2020-12-18 23:26:57 -0800

    More tooltips stuff, discord button copies discord instead of opening link

--- src/resources/js/components/text/Icon.vue
+++ src/resources/js/components/text/Icon.vue
@@ -1,17 +1,37 @@
 <template>
-    <span class="icon">
+    <span class="icon" ref="root" >
          <i v-bind:class="icon"></i>
     </span>
 </template>
 
 <script>
+import tippy from 'tippy.js';
+
 export default {
     name: "Icon",
     props: {
-        icon:{
+        icon: {
             type: String,
             required: true
         },
+        tooltip: {
+            type: String,
+            required: false,
+        },
+        tooltiptheme:{
+            type:String,
+            required: false
+        }
+
     },
+    mounted() {
+        if (this.tooltip) {
+            tippy(this.$refs.root, {
+                content: this.tooltip,
+                allowHTML: true,
+                theme: this.tooltiptheme,
+            });
+        }
+    }
 }
 </script>
