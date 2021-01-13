af4f3b316927ceedbcf656191253bde54b658ddb
DriedSponge <jordan@driedsponge.net>
2021-01-13 10:30:44 -0800

    Fixed db seeder to work with new authentication system

--- src/database/seeders/UserSeeder.php
+++ src/database/seeders/UserSeeder.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace Database\Seeders;
 
-use App\User;
+use App\Models\SocialAccounts;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -13,20 +15,22 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $key = config("steam-login.api_key");
-        $file = file_get_contents("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=$key&steamids=76561198357728256");
-        $steamUser = json_decode($file)->response->players[0];
-        if(!User::where('steamid',$steamUser->steamid)->first()){
+        if(!User::where('email',"ADMIN_EMAIL")->first()){
             $user = User::create([
                 'username' => "DriedSponge",
-                'steamid' => $steamUser->steamid,
-                'avatar' => $steamUser->avatarfull
+                'email' => config('app.adminemail'),
+                'avatar' => "https://avatars0.githubusercontent.com/u/47679132?s=460&u=3c95ff256901a64a085bc2237c389d177d041b09&v=4"
+            ]);
+
+            $socialaccount = SocialAccounts::create([
+               "provider"=>'discord',
+                "provider_id"=>"283710670409826304"
             ]);
             $user->assignRole('Owner');
+            $user->social_accounts()->save($socialaccount);
             echo "\nSeeded DriedSponge\n";
         }else{
             echo "\nNo user to seed\n";
         }
-
     }
 }
