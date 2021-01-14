c55c293c3ec89b4a52aad720c5e715109bbafa13
DriedSponge <jordan@driedsponge.net>
2021-01-13 18:28:58 -0800

    Better alert deleteing

--- src/app/Models/Alerts.php
+++ src/app/Models/Alerts.php
@@ -17,17 +17,30 @@ class Alerts extends Model
 
     public function deleteFull()
     {
-        if($this->tweetid){
-            $tweet = \Twitter::destroyTweet($this->tweetid);
-        }
+        $this->deleteTweet();
+        $this->deleteDiscord();
         $this->delete();
         return true;
     }
     public function deleteTweet()
     {
         if($this->tweetid){
-            $tweet = \Twitter::destroyTweet($this->tweetid);
+            try {
+                \Twitter::destroyTweet($this->tweetid);
+            }catch(\RuntimeException $e){}
             $this->tweetid = null;
+            $this->save();
+            return true;
+        }
+    }
+    public function deleteDiscord()
+    {
+        if($this->discordid){
+            $delete =\Http::delete(config('extra.discord_alerts_hook')."/messages/".$this->discordid);
+            if($delete->successful() or $delete->status() == 404){
+                $this->discordid = null;
+                $this->save();
+            }
             return true;
         }
     }
