9da0ed049bc163272f82d126db819b2361a79d1f
DriedSponge <jordan@driedsponge.net>
2021-01-13 17:11:54 -0800

    Added rest of alert creation code to job

--- src/app/Jobs/SendAlert.php
+++ src/app/Jobs/SendAlert.php
@@ -41,9 +41,47 @@ class SendAlert implements ShouldQueue
     {
         $alert = new Alerts();
         $alert->message = $this->message;
-        //$alert->twitter = $this->twitter;
-        //$alert->discord = $this->discord;
         $alert->onsite = $this->website;
+        if($this->twitter){
+            $messagearray = str_split ($this->message,271);
+            $count = count($messagearray);
+            if($count == 1){
+                $form = $messagearray[0];
+            }else{
+                $form = $messagearray[0]."... (1/$count)";
+            }
+            $tweet = \Twitter::postTweet(['status' => $form, 'format' => 'object']);
+            $tweetlink =  \Twitter::linkTweet($tweet);
+            $id=$tweet->id_str;
+            $alert->tweetid = $id;
+            $i = 0;
+            foreach ($messagearray as $chunk){
+                $i++;
+                $end = "... ($i/$count)";
+                if($i ==1){
+                    continue;
+                }elseif ($i==$count){
+                    $end = " ($i/$count)";
+                }
+                $tweet2 = \Twitter::postTweet(['status' =>  $chunk.$end, 'format' => 'json','in_reply_to_status_id'=>$id]);
+            }
+        }
+        if($this->discord){
+            $fields = array();
+            if($this->twitter){
+                array_push($fields, array("name" => "Tweet Link", "value" => $tweetlink));
+            }
+            $embed = array(
+                "title" =>  "New Message",
+                "type" => "rich",
+                "color" => hexdec("007BFF"),
+                "timestamp" => date("c"),
+                "fields" => $fields,
+                "description"=>$this->message
+            );
+            $response = \Http::post(config('extra.discord_alerts_hook')."?wait=true",["embeds" => [$embed]])->object();
+            $alert->discordid = $response->id;
+        }
         $alert->save();
 
     }
