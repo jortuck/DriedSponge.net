7d440efd0a5e6c7d5c8a24e1abb450b89ed5aaf7
DriedSponge <jordan@driedsponge.net>
2021-01-14 22:26:08 -0800

    Contact controller uses http post now instead of curl

--- src/app/Http/Controllers/ContactController.php
+++ src/app/Http/Controllers/ContactController.php
@@ -11,8 +11,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Http;
-use App\ContactResponses;
+use App\Models\ContactResponses;
 use Mailgun\Mailgun;
+
 class ContactController extends Controller
 {
     /**
@@ -44,40 +45,19 @@ class ContactController extends Controller
                 $data->Message = $request->message;
                 $data->save();
                 Mail::to('jordan@driedsponge.net')->send(new ContactForm($data));
-                try {
-                    $request = json_encode([
-                        "content" => "",
-                        "embeds" => [
-                            [
-                                "type" => "rich",
-                                "title"=>"New Contact Form Response",
-                                "description"=>"**Sender:** $data->Name ($data->Email)\n**Subject:** $data->Subject",
-                                "timestamp" => date("c"),
-                                "color"=> hexdec("00BE16"),
-                            ]
-                        ]
-                    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-                    $ch = curl_init(config('extra.discord_notification_hook'));
-                    curl_setopt_array($ch, [
-                        CURLOPT_POST => 1,
-                        CURLOPT_FOLLOWLOCATION => 1,
-                        CURLOPT_HTTPHEADER => array("Content-type: application/json"),
-                        CURLOPT_POSTFIELDS => $request,
-                        CURLOPT_RETURNTRANSFER => 1
-                    ]);
-                    curl_exec($ch);
-                    return response()->json(['success' => 'Your message has been sent!']);
-                } catch (Exception $e) {
-                    return response()->json(['error' => 'Message failed to send, please try again later']);
-                }
+                $embed = [
+                    "type" => "rich",
+                    "title" => "New Contact Form Response",
+                    "description" => "**Sender:** $data->Name ($data->Email)\n**Subject:** $data->Subject",
+                    "timestamp" => date("c"),
+                    "color" => hexdec("00BE16"),
+                ];
+                $response = \Http::post(config('extra.discord_notification_hook'), ["embeds" => [$embed]]);
+                return response()->json(['success' => 'Your message has been sent!']);
             } else {
                 return response()->json(['captcha' => 'Captcha failed, please try again.']);
             }
         }
         return response()->json($validator->errors());
-    }
-    public function testDatabase()
-    {
-        $model = factory(\App\ContactResponses::class,300)->make();
     }
 }
