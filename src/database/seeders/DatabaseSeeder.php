63991c1c8c67143d01905be73bceba47a9880dd2
DriedSponge <jordan@driedsponge.net>
2021-01-17 16:27:46 -0800

    Upload seeder works with laravel 8 now

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call([UploadSeeder::class]);
+        $this->call([UploadSeeder::class]);
         $this->call([RolesAndPermissionsSeeder::class]);
         $this->call([UserSeeder::class]);
     }
