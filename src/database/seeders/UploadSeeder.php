f5989b876c02bd5a0d01bc85473359ff2d53b792
DriedSponge <jordan@driedsponge.net>
2021-01-17 16:28:06 -0800

    Commiting the upload seeder changes?

--- src/database/seeders/UploadSeeder.php
+++ src/database/seeders/UploadSeeder.php
@@ -1,7 +1,10 @@
 <?php
+
 namespace Database\Seeders;
 
+use App\Models\FileUpload;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class UploadSeeder extends Seeder
 {
@@ -16,11 +19,11 @@ class UploadSeeder extends Seeder
             foreach (Storage::allFiles('/uploads') as $file){
                 $type = explode("/",Storage::mimeType($file))[1];
                 echo "\n".$file;
-                $data = new \App\FileUpload();
+                $data = new FileUpload();
                 $data->type  = $type;
                 $data->name =  explode("/",$file)[2];
                 $data->uuid = explode(".",$data->name)[0];
-                if(\App\FileUpload::where("name",$data->name)->first()){
+                if(FileUpload::where("name",$data->name)->first()){
                     echo "\n $file - File already exist!";
                     continue;
                 }
