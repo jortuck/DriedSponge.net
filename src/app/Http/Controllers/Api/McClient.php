e08634cb5f281736840e184579f1e6f3639b4c6e
DriedSponge <jordan@driedsponge.net>
2021-01-31 02:13:15 -0800

    Aparrently this file did not go with the last commit, I wish php storm worked better with WSL

---
+++ src/app/Http/Controllers/Api/McClient.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class McClient extends Controller
+{
+    public function index(){
+        die("test");
+    }
+}
