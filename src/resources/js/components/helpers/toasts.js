870a1e39ef4c4641b1a02edd5f2db483a9e3ce44
DriedSponge <jordan@driedsponge.net>
2021-02-13 09:21:55 -0800

    Fixed temp bug

--- src/resources/js/components/helpers/toasts.js
+++ src/resources/js/components/helpers/toasts.js
@@ -1,13 +1,5 @@
-import Toastify from 'toastify-js'
 
 
 export const toast = (className,message,duration = 5000) =>{
-    Toastify({
-        text: message,
-        className:className,
-        position:"center",
-        gravity:'top',
-        duration:duration,
-        close:true
-    }).showToast()
+    console.log("Tooptip")
 }
