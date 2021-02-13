f85dc46e221d291f9e38e1331ac41dd324fb6bd5
DriedSponge <jordan@driedsponge.net>
2021-02-13 09:47:31 -0800

    Added is-underlined method

---
+++ src/resources/js/components/text/Token.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="block">
+        {{msg}}
+        <br>
+        <a class="has-text-white is-underlined" href="#" @click="copy">
+            Copy The Token
+        </a>
+    </div>
+</template>
+
+<script>
+import tippy from "tippy.js";
+import Icon from "./Icon";
+
+export default {
+    name: "Token",
+    components: {Icon},
+    props:{
+        token:{
+            type: String,
+            required: true
+        },
+        msg:{
+            type: String,
+            required: true
+        }
+    },
+    methods:{
+        copy(e){
+            navigator.clipboard.writeText(this.token)
+            tippy(e.target, {
+                content: "Copied!",
+                onHidden(instance) {
+                    instance.destroy()
+                },
+            }).show();
+        }
+    }
+}
+</script>
