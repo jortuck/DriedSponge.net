a505be53120c0e30495237588f59e76c7dfdb45f
DriedSponge <jordan@driedsponge.net>
2021-03-05 08:15:20 -0800

    Updated login page

--- src/resources/js/views/public/views/Login.vue
+++ src/resources/js/views/public/views/Login.vue
@@ -2,25 +2,22 @@
     <section class="section">
         <div class="container">
             <div class="columns is-justify-content-center">
-                <div class="column is-5-desktop is-12-mobile is-9-tablet">
+                <div class="column is-6-desktop is-12-mobile is-10-tablet">
                     <div class="box" v-if="session.authenticated">
-                        <h2 @load="" class="title has-text-centered is-3 has-text-danger">You are already logged in!</h2>
+                        <h2 class="title has-text-centered is-3 has-text-danger">You are already logged in!</h2>
                     </div>
                     <div class="box" v-else>
-                        <h2 class="title has-text-centered is-3">Login With...</h2>
-                        <br>
-                            <div class="block has-text-danger has-text-centered has-text-weight-bold">
-                                This is just for me to login and access my dashboard. You can login if you want but it does absolutley nothing for you :).
-                            </div>
-                            <a href="/app/login/github" class="button is-large is-fullwidth is-light">
-                                <Icon icon="fab fa-github" class="is-left"/>
-                                <span>Github</span>
+                        <h2 class="title has-text-centered is-3">Login</h2>
+                        <p class="block has-text-danger has-text-centered has-text-weight-bold">
+                            This is just for me to login and access my dashboard. You can login if you want but it does
+                            absolutley nothing for you :).
+                        </p>
+                        <div class="buttons" v-for="provider in providers">
+                            <a :href="provider.url" class="button is-large is-fullwidth is-light">
+                                <Icon :icon="provider.icon" class="is-left"/>
+                                <span>{{ provider.name }}</span>
                             </a>
-                            <br>
-                            <a href="/app/login/discord" class="button is-large is-fullwidth is-light">
-                                <Icon icon="fab fa-discord" class="is-left"/>
-                                <span>Discord</span>
-                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -28,14 +25,18 @@
     </section>
 </template>
 <script>
-import Tileancestor from "../../../components/tiles/Tileancestor";
 import Icon from "../../../components/text/Icon";
+
 export default {
     name: "Login",
-    components: {Icon, Tileancestor},
-    data(){
-        return{
-            session: this.$store.state
+    components: {Icon},
+    data() {
+        return {
+            session: this.$store.state,
+            providers: [
+                {name: "Github", icon: "fab fa-github", url: "/app/login/github"},
+                {name: "Discord", icon: "fab fa-discord", url: "/app/login/discord"}
+            ]
         }
     }
 }
