f4ce468558225a666d986ed9695cd02777340e4f
DriedSponge <jordan@driedsponge.net>
2021-12-02 13:04:24 -0800

    Snow!

--- src/resources/js/main.js
+++ src/resources/js/main.js
@@ -10,12 +10,12 @@ import store from "./store";
 // axios settings
 axios.defaults.withCredentials = true;
 // Snow Storm Settings
-// require('./snowstorm-min');
-// window.snowStorm.followMouse = false
-// window.snowStorm.snowStick = false;
-// snowStorm.vMaxX = 5;
-// snowStorm.vMaxY = 5;
-// snowStorm.flakesMaxActive = 35;
+require('./snowstorm-min');
+window.snowStorm.followMouse = false
+window.snowStorm.snowStick = false;
+snowStorm.vMaxX = 5;
+snowStorm.vMaxY = 5;
+snowStorm.flakesMaxActive = 35;
 // Vue init
 const app = createApp(App).use(router).use(Toast,{
     timeout: 4000,
--- src/resources/js/views/public/Nav.vue
+++ src/resources/js/views/public/Nav.vue
@@ -1,6 +1,7 @@
 <template>
     <Navbar>
         <Navlink  v-for="nav in navlinks" :to="nav.to">{{nav.name}}</Navlink>
+        <Snowcontroller />
         <Navlogin />
     </Navbar>
 </template>
@@ -8,10 +9,12 @@
 import Navlogin from "../../components/includes/Navbar/Navlogin";
 import Navbar from "../../components/includes/Navbar/Navbar";
 import Navlink from "../../components/includes/Navbar/Navlink";
+import Snowcontroller from "../../components/includes/Navbar/Snowcontroller";
 
 export default {
     name: "Nav",
     components: {
+        Snowcontroller,
         Navlink,
         Navbar,
         Navlogin
