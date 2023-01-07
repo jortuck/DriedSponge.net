d9ab5fc6190ebb8f581996a63328b5b38d2b0199
DriedSponge <jordan@driedsponge.net>
2023-01-06 21:22:28 -0800

    Remove old laravel and vue stuff, it was a good run :(

--- src/artisan.sh
+++
@@ -1,1 +0,0 @@
-php artisan config:cache; php artisan route:cache; php artisan view:cache; php artisan migrate; php artisan key:generate;
--- src/routes/noweb.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::domain(config('extra.image_domain'))->group(function () {
-    Route::get('/file/{name}', 'FileUploads@loadFile')->name('upload.load-file');
-});
