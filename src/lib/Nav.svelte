cf8c10b9d560e1594355f124edf0970a2cc699be
DriedSponge <jordan@driedsponge.net>
2023-01-06 23:23:37 -0800

    Lets not get ahead of ourselves

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,11 @@
-<nav>
-    <ul>
-
-    </ul>
+<nav class="container mx-auto">
+    <div class="flex ">
+        <div>
+            <p>DS</p>
+        </div>
+        <div>
+            <span>Projects</span>
+            <span>Github</span>
+        </div>
+    </div>
 </nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
     import "../app.css";
+    import Nav from "$lib/Nav.svelte";
 </script>
 
 <main >
+    <Nav />
     <slot />
 </main>
