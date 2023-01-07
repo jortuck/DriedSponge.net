7945cf10088f8901fd0525523505dbe2acdd6c05
DriedSponge <jordan@driedsponge.net>
2023-01-06 21:55:48 -0800

    Lets get started

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome!</h1>
-<p class="text-red-500">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import PageHead from "$lib/PageHead.svelte";
+</script>
+
+<PageHead>
+    DRIEDSPONGE
+</PageHead>
