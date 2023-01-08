b38da05d2a432c07a5a749bd390ed8b51bdc410a
DriedSponge <jordan@driedsponge.net>
2023-01-07 18:46:44 -0800

    Added prettier

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,11 +1,30 @@
-<nav class="container mx-auto">
-    <div class="flex ">
-        <div>
-            <p>DS</p>
-        </div>
-        <div>
-            <span>Projects</span>
-            <span>Github</span>
-        </div>
-    </div>
+<nav class="container mx-auto py-4">
+	<div class="flex justify-between items-center">
+		<div>
+			<a
+				class="nav-logo"
+				href="/">DS</a
+			>
+		</div>
+		<div class="space-x-2">
+			<a
+				href="/projects"
+				class="nav-item">Projects</a
+			>
+			<a
+				class="nav-item"
+				href="https://github.com/driedsponge"
+				target="_blank">Github</a
+			>
+		</div>
+	</div>
 </nav>
+
+<style lang="postcss">
+	.nav-item {
+		@apply text-white text-lg;
+	}
+	.nav-logo {
+		@apply text-3xl;
+	}
+</style>
