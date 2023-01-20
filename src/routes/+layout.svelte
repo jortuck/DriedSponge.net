cf7d0f090ea36e45e4277d2cb866c53babdb6405
DriedSponge <jordan@driedsponge.net>
2023-01-19 16:59:35 -0800

    Added dev banner that can be dismissed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,15 +10,48 @@
 	import { onMount } from "svelte";
 	import { browser } from "$app/environment";
 
+	let hideBanner = true;
+
 	onMount(() => {
 		// TODO: Switch to svelte transitions once they allow for animations on load!
 		if (browser) {
 			AOS.init();
 			window.AOS = AOS;
 		}
+
+		if (!localStorage.getItem("hideBanner")) {
+			hideBanner = false;
+		}
 	});
+
+	function dismiss() {
+		hideBanner = true;
+		localStorage.setItem("hideBanner", "true");
+	}
 </script>
 
+{#if !hideBanner}
+	<div
+		class="bg-amber-700 p-4 text-center"
+		data-aos="fade-down"
+	>
+		<p class=" font-extrabold text-white">
+			This website is still a work in progress. Follow the development <a
+				class="underline transition-colors duration-200 ease-in-out hover:text-gray-300"
+				target="_blank"
+				rel="noreferrer"
+				href="https://github.com/driedsponge/DriedSponge.net">here</a
+			>.
+			<span
+				on:click={dismiss}
+				class="float-none cursor-pointer font-extrabold transition-colors duration-200 ease-in-out hover:text-gray-300 md:float-right"
+				title="Dismiss"
+			>
+				<i class="fa-regular fa-x fa-xl" />
+			</span>
+		</p>
+	</div>
+{/if}
 <Nav />
 <div class="max-w-8xl container mx-auto px-3 xl:px-0">
 	<slot />
