e2ef900956141d145e2331c99057fdb53399554a
jordan <jordan@driedsponge.net>
2023-11-14 22:41:32 -0800

    Fixed bug

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,8 @@
 import type { Actions } from "./$types";
 import { fail } from "@sveltejs/kit";
 import { contactSchema } from "$lib/Validator";
-import type { ValidationError } from "yup";
 import axios from "axios";
 import { TURNSTILE_SECRET_KEY, MAILGUN_KEY, MAILGUN_DOMAIN, EMAIL } from "$env/static/private";
-import formData from "form-data";
-import Mailgun from "mailgun.js";
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
@@ -36,24 +33,20 @@ export const actions = {
 				error: errors
 			});
 		}
-		const mailgun = new Mailgun(formData);
-		const client = mailgun.client({ username: "api", key: MAILGUN_KEY });
-		const messageData = {
-			from: `${name} <${email}>`,
-			to: EMAIL,
-			subject: "Contact From - " + subject,
-			text: message
-		};
-
-		client.messages
-			// @ts-ignore
-			.create(MAILGUN_DOMAIN, messageData)
-			.then((res) => {
-				console.log(res);
-			})
-			.catch((err) => {
-				console.error(err);
-			});
+		const body = new URLSearchParams();
+		body.append("to", EMAIL);
+		body.append("from", `${name} <${email}>`);
+		body.append("subject", "Contact From - " + subject);
+		body.append("html", `test`);
+		const res = await fetch(`https://api.mailgun.net/v3/${MAILGUN_DOMAIN}/messages`, {
+			method: "POST",
+			body: body.toString(),
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded",
+				Authorization: "Basic " + btoa(`api:${MAILGUN_KEY}`)
+			}
+		});
+		console.log(res.status);
 		return { success: true };
 	}
 } satisfies Actions;
