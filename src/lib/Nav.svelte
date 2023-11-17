e47b7da9587d41c0f2009c9f6a3d174f665a56e3
jordan <jordan@driedsponge.net>
2023-11-17 13:41:11 -0800

    Fix little nav issue

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -64,6 +64,13 @@
 					href="#projects"
 					class:text-gray-400={active !== "projects"}
 					class:text-white={active === "projects"}
+					on:click|preventDefault={() => {
+						scrollTo(
+							document.getElementById("projects").getBoundingClientRect().top +
+								window.pageYOffset -
+								60
+						);
+					}}
 				>
 					Projects
 				</a>
@@ -72,6 +79,13 @@
 					href="#contact"
 					class:text-gray-400={active !== "contact"}
 					class:text-white={active === "contact"}
+					on:click|preventDefault={() => {
+						scrollTo(
+							document.getElementById("contact").getBoundingClientRect().top +
+								window.pageYOffset -
+								60
+						);
+					}}
 				>
 					Contact
 				</a>
