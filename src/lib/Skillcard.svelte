b40031c51c9f32b65c1c1b0528ae2b53445e037e
DriedSponge <jordan@driedsponge.net>
2023-11-24 09:35:15 -0800

    Fade up animation on skill cards

--- src/lib/Skillcard.svelte
+++ src/lib/Skillcard.svelte
@@ -2,7 +2,10 @@
 	export let header: string = "";
 </script>
 
-<div class="relative w-full rounded-lg bg-bgsecondary p-5">
+<div
+	data-observe="fadeUp"
+	class="fadeUp relative w-full rounded-lg bg-bgsecondary p-5 transition-all duration-[1500ms] ease-in-out"
+>
 	{#if header !== ""}
 		<h1 class="text-center text-2xl font-bold text-white">{header}</h1>
 	{/if}
@@ -10,3 +13,9 @@
 		<slot />
 	</p>
 </div>
+
+<style lang="postcss">
+	.fadeUp {
+		@apply opacity-0 lg:translate-y-[200px];
+	}
+</style>
