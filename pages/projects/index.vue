74f4ce5500bc3c5a81097679f757afb403c5d31c
Jordan <jordan@driedsponge.net>
2024-12-23 13:27:14 -0800

    more adjustments

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -5,34 +5,22 @@ useSeoMeta({
 </script>
 <template>
 	<section>
-		<h1 class="text-white text-6xl my-10 font-bold">Projects</h1>
+		<h1 class="text-white md:text-6xl text-5xl my-10 font-bold">Projects</h1>
 		<ContentList path="/projects" v-slot="{list}">
 			<div class="grid md:grid-cols-2 grid-cols-1 gap-3 my-3">
-				<div v-for="item in list" class="bg-base-200 rounded-md">
-					<div class="my-4 mx-2 space-y-1">
-						<h1 class="text-white text-2xl font-bold">
+				<NuxtLink :to="item._path" v-for="item in list" class="block bg-base-100 rounded-md flex flex-col border-base-200 hover:border-base-300 border-2">
+					<div class="m-2 space-y-1 flex-1">
+						<a class="text-2xl font-bold text-primary">
 							{{ item.title }}
-						</h1>
+						</a>
 						<p class="text-white">
 							{{ item.description }}
 						</p>
 					</div>
-					<div
-						:style="`background-image: url(/thumbnails/${item.thumbnail})`"
-						class="h-52 bg-white md:h-80"
-					>
-						<div class="relative h-full backdrop-blur-2xl">
-							<img
-								alt="{imgAlt}"
-								class="relative z-40 mx-auto h-full object-contain"
-								:src="`/thumbnails/${item.thumbnail}`"
-							/>
-						</div>
+					<div class="text-white text-2xl m-2 space-x-2.5">
+						<i class="fa-brands fa-github"></i><i class="fa-solid fa-globe"></i>
 					</div>
-					<a href="#" class="bg-primary text-center text-white py-2 font-bold block overflow-hidden rounded-b-md">
-						Learn More
-					</a>
-				</div>
+				</NuxtLink>
 			</div>
 		</ContentList>
 	</section>
