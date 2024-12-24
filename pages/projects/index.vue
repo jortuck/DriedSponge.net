5238cf32eccdc75ab40b957392e452c2eef5741e
Jordan <jordan@driedsponge.net>
2024-12-24 02:02:34 -0800

    add bottom margin to projects list for mobile

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -5,7 +5,7 @@ useSeoMeta({
 });
 </script>
 <template>
-	<section>
+	<section class="mb-10">
 		<h1 class="text-white md:text-6xl text-5xl my-10 font-bold">Projects</h1>
 		<ContentList path="/projects" v-slot="{list}">
 			<div class="grid md:grid-cols-2 grid-cols-1 gap-3 my-3">
