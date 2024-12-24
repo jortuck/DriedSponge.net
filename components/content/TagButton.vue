2fad35761c021b8deb196bf688e6e9f533a38f60
Jordan <jordan@driedsponge.net>
2024-12-24 02:08:07 -0800

    better tag buttons

--- components/content/TagButton.vue
+++ components/content/TagButton.vue
@@ -6,7 +6,7 @@ defineProps({
 })
 </script>
 <template>
-	<NuxtLink external target="_blank" :to="url" class="p-3 bg-base-200 hover:bg-base-300 text-white rounded-md font-bold md:text-base text-2xl w-full text-center space-x-1.5">
-		<i :class="icon"></i><span class="hidden md:inline">{{text}}</span>
+	<NuxtLink external target="_blank" :to="url" class="px-3 md:py-3 py-2 bg-base-200 hover:bg-base-300 text-white rounded-md font-bold text-base w-full text-center space-x-1.5">
+		<i :class="icon"></i><span>{{text}}</span>
 	</NuxtLink>
 </template>
--- components/content/TagButtonGroup.vue
+++ components/content/TagButtonGroup.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="space-x-2.5 flex flex-row">
+	<div class="md:space-x-2.5 md:flex-row space-y-3 md:space-y-0 flex flex-col">
 		<ContentSlot :use="$slots.default" unwrap="p" />
 	</div>
 </template>
