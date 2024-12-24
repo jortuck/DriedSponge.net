0b1bec1e6e6b54a8dfbf8fc92607a959fe2b8266
Jordan <jordan@driedsponge.net>
2024-12-24 01:58:22 -0800

    underline back to projects

--- pages/projects/[...slug].vue
+++ pages/projects/[...slug].vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="my-10 space-y-4">
-		<NuxtLink href="/projects" class="text-white rounded-md un"><i class="fa-solid fa-arrow-left"></i> Back To Projects</NuxtLink>
+		<NuxtLink href="/projects" class="text-white rounded-md hover:underline"><i class="fa-solid fa-arrow-left"></i> Back To Projects</NuxtLink>
 		<ContentDoc class="project-content" />
 	</section>
 </template>/
