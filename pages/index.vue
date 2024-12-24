d793b110314192161c04a994e93ca87a632352fd
Jordan <jordan@driedsponge.net>
2024-12-23 20:19:27 -0800

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
 import Shield from "~/components/Shield.vue";
 
 useSeoMeta({
-	title: "Home",
-	ogTitle: "Home",
+	title: "Home | Jordan Tucker",
+	ogTitle: "Home | Jordan Tucker",
 	description: "Hello, my name is Jordan. I'm a student studying at the University of Washington. I enjoy creating websites, Discord bots, & more!",
 	ogDescription: "Hello , my name is Jordan. I'm a student studying at the University of Washington. I enjoy creating websites, Discord bots, & more!",
 	ogUrl: "https://jortuck.com"
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -8,19 +8,26 @@ useSeoMeta({
 		<h1 class="text-white md:text-6xl text-5xl my-10 font-bold">Projects</h1>
 		<ContentList path="/projects" v-slot="{list}">
 			<div class="grid md:grid-cols-2 grid-cols-1 gap-3 my-3">
-				<NuxtLink :to="item._path" v-for="item in list" class="block bg-base-100 rounded-md flex flex-col border-base-200 hover:border-base-300 border-2">
-					<div class="m-2 space-y-1 flex-1">
-						<a class="text-2xl font-bold text-primary">
-							{{ item.title }}
-						</a>
+				<div v-for="item in list" class="rounded-md flex flex-col border-base-300 border-2">
+					<div class="m-2 space-y-2 flex-1">
+						<div class="space-x-2">
+							<NuxtLink :to="item._path" class="text-2xl font-bold text-primary hover:underline">
+								{{ item.title }}
+							</NuxtLink>
+						</div>
 						<p class="text-white">
 							{{ item.description }}
 						</p>
 					</div>
-					<div class="text-white text-2xl m-2 space-x-2.5">
-						<i class="fa-brands fa-github"></i><i class="fa-solid fa-globe"></i>
+					<div class="text-white m-2 space-x-2.5">
+						<NuxtLink :to="`${item._path}`" class="space-x-2.5 bg-base-200 hover:bg-base-300 py-1 px-2 rounded-md text-sm select-none">
+							View Project <i class="fa-solid fa-arrow-right"></i>
+						</NuxtLink>
+						<NuxtLink target="_blank" external :to="`https://github.com/jortuck/${item.github}`" class="space-x-2.5 bg-base-200 hover:bg-base-300 py-1 px-2 rounded-md  text-sm select-none">
+						Source Code <i class="fa-regular fa-arrow-up-right-from-square ml-1 fa-sm"></i>
+						</NuxtLink>
 					</div>
-				</NuxtLink>
+				</div>
 			</div>
 		</ContentList>
 	</section>
