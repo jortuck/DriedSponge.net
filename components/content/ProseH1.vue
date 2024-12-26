f150035b2a2f952d876e5633793bca258dd412b7
Jordan <jordan@driedsponge.net>
2024-12-25 18:30:15 -0800

    use source prose h1

--- components/content/ProseH1.vue
+++ components/content/ProseH1.vue
@@ -1,5 +1,20 @@
 <template>
-	<h1 class=" md:text-6xl text-4xl font-bold bg-gradient-to-br from-white via-primary to-white bg-clip-text text-transparent">
-		<slot/>
+	<h1 :id="props.id" class="md:text-6xl text-4xl font-bold bg-gradient-to-br from-white via-primary to-white bg-clip-text text-transparent">
+		<a
+			v-if="generate"
+			:href="`#${props.id}`"
+		>
+			<slot />
+		</a>
+		<slot v-else />
 	</h1>
 </template>
+
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from '#imports'
+
+const props = defineProps<{ id?: string }>()
+
+const { headings } = useRuntimeConfig().public.mdc
+const generate = computed(() => props.id && ((typeof headings?.anchorLinks === 'boolean' && headings?.anchorLinks === true) || (typeof headings?.anchorLinks === 'object' && headings?.anchorLinks?.h1)))
+</script>
