748aca40a48d7e8a65412fd23b2a2d893c40f239
Jordan <jordan@driedsponge.net>
2024-12-26 00:08:52 -0800

    coment out email stuff, does fix?

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -23,21 +23,21 @@ export default defineEventHandler(async (event) => {
 		setResponseStatus(event, 403);
 		return {message:"Unfortunately my system has detected you as a bot. Please refresh your page or reach out via LinkedIn."}
 	}
-	const transporter = createTransport({
-		host: useRuntimeConfig().mailHost,
-		port: 465,
-		secure: true,
-		auth: {
-			user: useRuntimeConfig().mailUser,
-			pass: useRuntimeConfig().mailPassword,
-		}
-	})
-	const info = await transporter.sendMail({
-		from: '"Contact Form" <noreply@jortuck.com>', // sender address
-		replyTo:`"${result.data.name}" <${result.data.email}>"`, // reply ro address
-		to: useRuntimeConfig().mailDestination, // list of receivers
-		subject: result.data.subject, // Subject line
-		text: result.data.message, // plain text body
-	});
+	// const transporter = createTransport({
+	// 	host: useRuntimeConfig().mailHost,
+	// 	port: 465,
+	// 	secure: true,
+	// 	auth: {
+	// 		user: useRuntimeConfig().mailUser,
+	// 		pass: useRuntimeConfig().mailPassword,
+	// 	}
+	// })
+	// const info = await transporter.sendMail({
+	// 	from: '"Contact Form" <noreply@jortuck.com>', // sender address
+	// 	replyTo:`"${result.data.name}" <${result.data.email}>"`, // reply ro address
+	// 	to: useRuntimeConfig().mailDestination, // list of receivers
+	// 	subject: result.data.subject, // Subject line
+	// 	text: result.data.message, // plain text body
+	// });
 	return {success:true,message:"Your message has been sent!"}
 });
