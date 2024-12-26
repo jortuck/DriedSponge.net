fff658db0cd989d67b258b922cfeb1ab1ef3b22f
Jordan <jordan@driedsponge.net>
2024-12-26 01:53:46 -0800

    AWS I AM BEGGING PLEASE WORK

--- server/api/contact.post.ts
+++ server/api/contact.post.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { schema} from "#shared/ContactFormScheme";
 import { $fetch } from "ofetch";
+import { SendEmailCommand, SES, SESClient } from "@aws-sdk/client-ses";
 
 export default defineEventHandler(async (event) => {
 	let config = useRuntimeConfig(event);
@@ -24,7 +25,68 @@ export default defineEventHandler(async (event) => {
 		return {message:"Unfortunately my system has detected you as a bot. Please refresh your page or reach out via LinkedIn."}
 	}
 
+	const createSendEmailCommand = (toAddress:string, fromAddress:string) => {
+		return new SendEmailCommand({
+			Destination: {
+				/* required */
+				CcAddresses: [
+					/* more items */
+				],
+				ToAddresses: [
+					toAddress,
+					/* more To-email addresses */
+				],
+			},
+			Message: {
+				/* required */
+				Body: {
+					/* required */
+					Html: {
+						Charset: "UTF-8",
+						Data: "HTML_FORMAT_BODY",
+					},
+					Text: {
+						Charset: "UTF-8",
+						Data: "TEXT_FORMAT_BODY",
+					},
+				},
+				Subject: {
+					Charset: "UTF-8",
+					Data: "EMAIL_SUBJECT",
+				},
+			},
+			Source: fromAddress,
+			ReplyToAddresses: [
+				/* more items */
+			],
+		});
+	};
+	let sesClient = new SESClient({
+		region:"us-east-1",
+		credentials:{
+			accessKeyId:config.mailUser,
+			secretAccessKey:config.mailPassword
+		}
+	})
 
+	const run = async () => {
+		const sendEmailCommand = createSendEmailCommand(
+			"jordan@jortuck.com",
+			"noreply@jortuck.com",
+		);
+
+		try {
+			return await sesClient.send(sendEmailCommand);
+		} catch (caught) {
+			if (caught instanceof Error && caught.name === "MessageRejected") {
+				/** @type { import('@aws-sdk/client-ses').MessageRejected} */
+				const messageRejectedError = caught;
+				return messageRejectedError;
+			}
+			throw caught;
+		}
+	};
+	await run();
 	// const transporter = createTransport({
 	// 	host: useRuntimeConfig().mailHost,
 	// 	port: 465,
