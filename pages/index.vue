2c809f14e97dfeea0d7e665cb6e29a700afb0c74
Jordan <jordan@driedsponge.net>
2024-12-27 13:34:29 -0800

    add more education and work blocks

--- pages/index.vue
+++ pages/index.vue
@@ -194,30 +194,11 @@ async function handleSubmit() {
 				<ProseH3>Work</ProseH3>
 				<div class="my-3 grid grid-cols-1 gap-3 md:grid-cols-2">
 					<div
-						class="from-base-150 flex flex-col rounded-md border-2 border-base-300 bg-gradient-to-br to-base-100"
+						class="from-base-150 flex flex-col rounded-md border-2 border-base-200 bg-gradient-to-br to-base-100"
 					>
 						<div class="m-2 flex-1 space-y-2">
 							<div class="space-x-2">
-								<h4 class="text-xl font-bold text-white md:text-2xl">
-									Undergraduate Teaching Assistant
-								</h4>
-							</div>
-							<p class="text-white">Teaching</p>
-						</div>
-						<div class="m-2 space-x-2.5 text-white">
-							<span
-								class="select-none space-x-2.5 rounded-md bg-base-200 px-2 py-1 text-sm hover:bg-base-300"
-							>
-								Something <i class="fa-solid fa-arrow-right"></i>
-							</span>
-						</div>
-					</div>
-					<div class="flex flex-col rounded-md border-2 border-base-300">
-						<div class="m-2 flex-1 space-y-2">
-							<div class="space-x-2">
-								<h4 class="text-xl font-bold text-white md:text-2xl">
-									Undergraduate Research Assistant
-								</h4>
+								<h4 class="text-xl font-bold text-white">Undergraduate Teaching Assistant</h4>
 							</div>
 							<p class="text-white">Teaching</p>
 						</div>
@@ -231,6 +212,29 @@ async function handleSubmit() {
 					</div>
 				</div>
 				<ProseH3>Education</ProseH3>
+				<div class="flex flex-row">
+					<div class="flex-0 bg-husky flex rounded-l-md py-10 align-middle">
+						<img
+							class="mx-auto w-1/2"
+							alt="University of Washington logo."
+							src="~/assets/images/uwlogo.png"
+						/>
+					</div>
+					<div
+						class="from-base-150 w-full rounded-br-md rounded-tr-md border-y-2 border-r-2 border-base-200 bg-gradient-to-br to-base-100"
+					>
+						<div class="m-3 space-y-2">
+							<h4 class="text-xl font-bold text-white">
+								Bachelor of Science - Informatics, Expected June 2027
+							</h4>
+							<ul class="list-inside list-disc text-white">
+								<li>Minor in Mathematics.</li>
+								<li>Member of the Husky Marching Band.</li>
+								<li>Deans List Recipient: Autumn 2023, Winter 2024, Spring 2024, Autumn 2024.</li>
+							</ul>
+						</div>
+					</div>
+				</div>
 				<ProseH3>Projects</ProseH3>
 			</div>
 			<div class="space-y-4">
